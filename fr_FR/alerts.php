03e4c3aa9441df682f07dc25f4adb0c8f69f3945
philoticus <phil.atale@gmail.com>
2011-12-15 14:59:02 +0100

    Update fr_FR/alerts.php

--- fr_FR/alerts.php
+++ fr_FR/alerts.php
@@ -2,7 +2,7 @@
 	$lang = array(
 	'alert_email' => array(
 		'email' => 'Adresse email invalide.',
-		'email_check' => 'Cette adress email a déjà été enregistrée pour les alertes sur ce lieu',
+		'email_check' => 'Cette adresse email a déjà été enregistrée pour les alertes sur ce lieu',
 		'required' => 'Une adresse email est requise si la case est cochée.',
 	),
 	'alert_lat' => array(
@@ -14,10 +14,10 @@
 		'required' => 'Position sur la carte invalide.',
 	),
 	'alert_mobile' => array(
-		'length' => 'Le numéro de téléphone de comporte pas le bon nombre de chiffres',
+		'length' => 'Le numéro de téléphone ne comporte pas le bon nombre de chiffres',
 		'mobile_check' => 'Ce numéro de téléphone a déjà été enregistré pour les alertes sur ce lieu.',
 		'numeric' => 'Numéro de téléphone invalide. Entrez uniquement des chiffres, sans oublier le préfixe international.',
-		'one_required' => 'Précisez soit voitre numéro de téléphone portable, soit votre adresse email.',
+		'one_required' => 'Précisez soit votre numéro de téléphone portable, soit votre adresse email.',
 		'required' => 'Un numéro de téléphone est requis si la case est cochée.',
 	));
 ?>
